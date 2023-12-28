9503a87c7f19c2985a74ba44dc42c33e41e9fad9
Smartbro789 <smartbro789@gmail.com>
2023-12-28 21:24:17 +0200

    Update UserBusiness.ts

--- Back-end/src/busines/UserBusiness.ts
+++ Back-end/src/busines/UserBusiness.ts
@@ -33,7 +33,7 @@ export class UserBusiness{
                 surname,
                 position,
                 dob,
-                ,
+                passport,
                 salary
             }
 
